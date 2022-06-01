c7ba11f40dce55520a3af59494bde45d1490373c
Samuel Cahyawijaya <scahyawijaya@connect.ust.hk>
2022-06-01 23:42:27 +0800

    add bapos dataset, add common parser for conll data, add constants for Pos tagging, update test script

---
+++ nusantara/utils/common_parser.py
@@ -0,0 +1,24 @@
+import os, sys
+import json
+import pickle
+
+def load_conll_data(file_path):
+    # Read file
+    data = open(file_path,'r').readlines()
+
+    # Prepare buffer
+    dataset = []
+    sentence, seq_label = [], []
+    for line in data:
+        if len(line.strip()) > 0:
+            token, label = line[:-1].split('\t')
+            sentence.append(token)
+            seq_label.append(label)
+        else:
+            dataset.append({
+                'sentence': sentence,
+                'label': seq_label
+            })
+            sentence = []
+            seq_label = []
+    return dataset
