918d7f041f14a6ed39a40d0308c4d807c0f11a97
James Jaya <jamesjaya@fb.com>
2022-06-26 09:09:39 +0800

    Remove __main__ from id_clickbait.py

--- nusantara/nusa_datasets/id_clickbait/id_clickbait.py
+++ nusantara/nusa_datasets/id_clickbait/id_clickbait.py
@@ -121,13 +121,3 @@ class IdClickbait(datasets.GeneratorBasedBuilder):
                     "labels": [row["label"]],
                 }
                 yield row_index, ex
-
-
-# This template is based on the following template from the datasets package:
-# https://github.com/huggingface/datasets/blob/master/templates/new_dataset_script.py
-
-
-# This allows you to run your dataloader with `python [dataset_name].py` during development
-# TODO: Remove this before making your PR
-if __name__ == "__main__":
-    datasets.load_dataset(__file__)
