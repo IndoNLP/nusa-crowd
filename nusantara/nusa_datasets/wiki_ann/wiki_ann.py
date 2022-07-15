5a082b962836f43bcbff6c4803921a00594295c4
Kaustubh Dhole <caxton007.john@gmail.com>
2022-07-15 01:13:35 -0400

    Update description

--- nusantara/nusa_datasets/wiki_ann/wiki_ann.py
+++ nusantara/nusa_datasets/wiki_ann/wiki_ann.py
@@ -62,7 +62,7 @@ The wiki_ann dataset contains NER tags with labels from O (0), B-PER (1), I-PER 
 WikiANN (sometimes called PAN-X) is a multilingual named entity recognition dataset consisting of Wikipedia articles
  annotated with LOC (location), PER (person), and ORG (organisation)
  tags in the IOB2 format. This version corresponds to the balanced train, dev, and test splits of
-  Rahimi et al. (2019), which supports 176 of the 282 languages from the original WikiANN corpus.
+  Rahimi et al. (2019), and uses the Indonesian subset from the original WikiANN corpus.
 """
 
 _HOMEPAGE = "https://github.com/afshinrahimi/mmner"
