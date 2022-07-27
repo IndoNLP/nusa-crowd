9c0b561b69d40e1daa04991076cf19be32fcad75
Muhammad Satrio Wicaksono <muhsatrio@gmail.com>
2022-07-27 22:16:26 +0700

    chore: improve test_example.sh for the cross fold

--- test_example.sh
+++ test_example.sh
@@ -1,8 +1,17 @@
 if [[ "$1" == "" ]]; then
     echo "Error: Missing the dataset name"
-    echo "./test_example.sh <dataset name>"
+    echo "./test_example.sh <dataset name> <number of cross fold (optional)>"
     exit
 fi
 
 # Test for inputed dataset
+
+if [[ "$2" -ne "" ]]; then
+    for ((i=0; i < $2; i++ ));
+    do
+        python -m tests.test_nusantara nusantara/nusa_datasets/$1/$1.py --subset_id "$1_fold$i"
+    done
+    exit
+fi
+
 python -m tests.test_nusantara nusantara/nusa_datasets/$1/$1.py
