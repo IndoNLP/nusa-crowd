dc26dc66d52e6c665392ac57a5b502fd6794b872
rayendito <13519157@std.stei.itb.ac.id>
2022-07-29 22:21:24 +0700

    Tests OK

--- nusantara/nusa_datasets/facqa/utils/facqa_utils.py
+++ nusantara/nusa_datasets/facqa/utils/facqa_utils.py
@@ -20,10 +20,3 @@ def getAnswerString(list_of_string, answer_mask) -> str:
             answer_string += " " + list_of_string[i]
 
     return answer_string
-
-# passage1 = ['Jimmy', 'Arnold', '(', '54', ')', ',', 'pencipta', 'Kursi', 'Stroke', '(', 'Stroke', 'Muscle', 'Therapy', ')', ',', 'Rabu', '(', '24/8', ')', 'siang', ',', 'menyatakan', ',', 'seorang', 'karyawan', 'perusahaan', 'minyak', 'AS', 'di', 'Jakarta', 'yakni', 'Jary', 'Wayland', '(', '60', ')', 'sangat', 'tertarik', 'akan', 'kursi', 'buatannya', '.']
-# answerm1 = ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'B', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O']
-
-# print(listToString(passage1))
-# print()
-# print(getAnswerString(passage1, answerm1))
