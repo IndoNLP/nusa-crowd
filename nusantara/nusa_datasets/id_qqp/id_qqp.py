c2768a69a3f06465522fe2bc6a8b11a7006c9298
yana-xuyan <278604253@qq.com>
2022-08-15 19:47:45 +0800

    rm \url

--- nusantara/nusa_datasets/id_qqp/id_qqp.py
+++ nusantara/nusa_datasets/id_qqp/id_qqp.py
@@ -13,7 +13,7 @@ _CITATION = """\
 @misc{quoraFirstQuora,
 	author = {},
 	title = {{F}irst {Q}uora {D}ataset {R}elease: {Q}uestion {P}airs --- quoradata.quora.com},
-	howpublished = {\url{https://quoradata.quora.com/First-Quora-Dataset-Release-Question-Pairs}},
+	howpublished = {https://quoradata.quora.com/First-Quora-Dataset-Release-Question-Pairs},
 	year = 2017,
 	note = {Online},
 }
