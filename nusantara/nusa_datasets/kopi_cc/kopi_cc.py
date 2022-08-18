0cdbb7a3a91b594a4feb5b72b414651de9587b8d
munggok <samsulrahmadani@gmail.com>
2022-08-18 13:32:11 +0000

    remove desc cc100

--- nusantara/nusa_datasets/kopi_cc/kopi_cc.py
+++ nusantara/nusa_datasets/kopi_cc/kopi_cc.py
@@ -14,16 +14,6 @@
 # limitations under the License.
 
 """
-This corpus is an attempt to recreate the dataset used for training XLM-R. This
-corpus comprises of monolingual data for 100+ languages and also includes data
-for romanized languages (indicated by *_rom). This was constructed using the
-urls and paragraph indices provided by the CC-Net repository by processing
-January-December 2018 Commoncrawl snapshots. Each file comprises of documents
-separated by double-newlines and paragraphs within the same document separated
-by a newline. The data is generated using the open source CC-Net repository. No
-claims of intellectual property are made on the work of preparation of the
-corpus.
-
 This contains the Indonesian (ind), the Javanese (jav), and the Sundanese (sun) subset.
 
 [nusantara_schema_name] = ssp
