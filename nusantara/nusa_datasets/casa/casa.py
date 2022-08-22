45fba02ab0ac4e6bc1f6f5263cfa820e6f08d6ee
FaridLazuarda <faridadilazuarda29@gmail.com>
2022-08-22 21:58:50 +0700

    remove unnecesary line

--- nusantara/nusa_datasets/casa/casa.py
+++ nusantara/nusa_datasets/casa/casa.py
@@ -149,7 +149,3 @@ class CASA(datasets.GeneratorBasedBuilder):
                     "labels": [label for label in row[3:]],
                 }
                 yield row.index, entry
-
-
-if __name__ == "__main__":
-    datasets.load_dataset(__file__)
