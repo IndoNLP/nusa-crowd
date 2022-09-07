f536f5736a5a0f227099c35b2b4d4da270a76e81
sudan <sudan0317@gmail.com>
2022-09-07 10:29:08 +0800

    Add multilexnorm dataset

---
+++ nusantara/nusa_datasets/jv_id/jv_id.py
@@ -0,0 +1,186 @@
+import csv
+import os
+from pathlib import Path
+from typing import List
+
+import datasets
+
+from nusantara.utils import schemas
+from nusantara.utils.configs import NusantaraConfig
+from nusantara.utils.constants import (DEFAULT_NUSANTARA_VIEW_NAME,
+                                       DEFAULT_SOURCE_VIEW_NAME, Tasks)
+
+_DATASETNAME = "jv_id"
+_SOURCE_VIEW_NAME = DEFAULT_SOURCE_VIEW_NAME
+_UNIFIED_VIEW_NAME = DEFAULT_NUSANTARA_VIEW_NAME
+
+_LANGUAGES = ["ind"]  # We follow ISO639-3 language code (https://iso639-3.sil.org/code_tables/639/data)
+_LOCAL = False
+_CITATION = """\
+@inproceedings{sodimana18_sltu,
+  author={Keshan Sodimana and Pasindu {De Silva} and Supheakmungkol Sarin and Oddur Kjartansson and Martin Jansche and Knot Pipatsrisawat and Linne Ha},
+  title={{A Step-by-Step Process for Building TTS Voices Using Open Source Data and Frameworks for Bangla, Javanese, Khmer, Nepali, Sinhala, and Sundanese}},
+  year=2018,
+  booktitle={Proc. 6th Workshop on Spoken Language Technologies for Under-Resourced Languages (SLTU 2018)},
+  pages={66--70},
+  doi={10.21437/SLTU.2018-14}
+}
+"""
+
+_DESCRIPTION = """\
+This data set contains high-quality transcribed audio data for Javanese.
+The data set consists of wave files, and a TSV file.
+The file line_index.tsv contains a filename and the transcription of audio in the file.
+Each filename is prepended with a speaker identification number.
+The data set has been manually quality checked, but there might still be errors.
+This dataset was collected by Google in collaboration with Gadjah Mada University in Indonesia.
+"""
+
+_HOMEPAGE = "https://indonlp.github.io/nusa-catalogue/card.html?jv_id_asr"
+
+_LICENSE = "See https://www.openslr.org/resources/41/LICENSE file for license information. Attribution-ShareAlike 4.0 (CC BY-SA 4.0)."
+
+_URLs = {
+    _DATASETNAME: {
+        "female": "https://www.openslr.org/resources/41/jv_id_female.zip",
+        "male": "https://www.openslr.org/resources/41/jv_id_male.zip",
+    }
+}
+
+_SUPPORTED_TASKS = [Tasks.SPEECH_RECOGNITION]
+
+_SOURCE_VERSION = "1.0.0"
+_NUSANTARA_VERSION = "1.0.0"
+
+
+class JvId(datasets.GeneratorBasedBuilder):
+    """jv_id contains high-quality Multi-speaker TTS data for Javanese (jv-ID)."""
+
+    BUILDER_CONFIGS = [
+        NusantaraConfig(
+            name="jv_id_source",
+            version=datasets.Version(_SOURCE_VERSION),
+            description="JV_ID source schema",
+            schema="source",
+            subset_id="jv_id",
+        ),
+        NusantaraConfig(
+            name="jv_id_nusantara_sptext",
+            version=datasets.Version(_NUSANTARA_VERSION),
+            description="JV_ID Nusantara schema",
+            schema="nusantara_sptext",
+            subset_id="jv_id",
+        ),
+    ]
+
+    DEFAULT_CONFIG_NAME = "jv_id_source"
+
+    def _info(self):
+        if self.config.schema == "source":
+            features = datasets.Features(
+                {
+                    "id": datasets.Value("string"),
+                    "speaker_id": datasets.Value("string"),
+                    "path": datasets.Value("string"),
+                    "audio": datasets.Audio(sampling_rate=16_000),
+                    "text": datasets.Value("string"),
+                }
+            )
+        elif self.config.schema == "nusantara_sptext":
+            features = schemas.speech_text_features
+
+        return datasets.DatasetInfo(
+            description=_DESCRIPTION,
+            features=features,
+            homepage=_HOMEPAGE,
+            license=_LICENSE,
+            citation=_CITATION,
+            task_templates=[datasets.AutomaticSpeechRecognition(audio_column="audio", transcription_column="text")],
+        )
+
+    def _split_generators(self, dl_manager: datasets.DownloadManager) -> List[datasets.SplitGenerator]:
+        male_path = Path(dl_manager.download_and_extract(_URLs[_DATASETNAME]["male"]))
+        female_path = Path(dl_manager.download_and_extract(_URLs[_DATASETNAME]["female"]))
+
+        return [
+            datasets.SplitGenerator(
+                name=datasets.Split.TRAIN,
+                gen_kwargs={
+                    "male_filepath": male_path,
+                    "female_filepath": female_path,
+                },
+            ),
+        ]
+
+    def _generate_examples(self, male_filepath: Path, female_filepath: Path):
+
+        if self.config.schema == "source" or self.config.schema == "nusantara_sptext":
+            tsv_file = os.path.join(male_filepath, "jv_id_male", "line_index.tsv")
+            with open(tsv_file, "r") as file:
+                tsv_data = csv.reader(file, delimiter="\t")
+
+                for line in tsv_data:
+                    # for male data, the tsv contains three columns
+                    audio_id, _, transcription_text = line[0], line[1], line[2]
+                    speaker_id = audio_id.split("_")[1]
+                    wav_path = os.path.join(male_filepath, "jv_id_male", "wavs", "{}.wav".format(audio_id))
+
+                    if os.path.exists(wav_path):
+                        if self.config.schema == "source":
+                            ex = {
+                                "id": audio_id,
+                                "speaker_id": speaker_id,
+                                "path": wav_path,
+                                "audio": wav_path,
+                                "text": transcription_text,
+                            }
+                            yield audio_id, ex
+                        elif self.config.schema == "nusantara_sptext":
+                            ex = {
+                                "id": audio_id,
+                                "speaker_id": speaker_id,
+                                "path": wav_path,
+                                "audio": wav_path,
+                                "text": transcription_text,
+                                "metadata": {
+                                    "speaker_age": None,
+                                    "speaker_gender": "male",
+                                },
+                            }
+                            yield audio_id, ex
+
+            tsv_file = os.path.join(female_filepath, "jv_id_female", "line_index.tsv")
+            with open(tsv_file, "r") as file:
+                tsv_data = csv.reader(file, delimiter="\t")
+
+                for line in tsv_data:
+                    audio_id, transcription_text = line[0], line[1]
+                    speaker_id = audio_id.split("_")[1]
+                    wav_path = os.path.join(female_filepath, "jv_id_female", "wavs", "{}.wav".format(audio_id))
+
+                    if os.path.exists(wav_path):
+                        if self.config.schema == "source":
+                            ex = {
+                                "id": audio_id,
+                                "speaker_id": speaker_id,
+                                "path": wav_path,
+                                "audio": wav_path,
+                                "text": transcription_text,
+                            }
+                            yield audio_id, ex
+                        elif self.config.schema == "nusantara_sptext":
+                            ex = {
+                                "id": audio_id,
+                                "speaker_id": speaker_id,
+                                "path": wav_path,
+                                "audio": wav_path,
+                                "text": transcription_text,
+                                "metadata": {
+                                    "speaker_age": None,
+                                    "speaker_gender": "male",
+                                },
+                            }
+                            yield audio_id, ex
+        else:
+            raise ValueError(f"Invalid config: {self.config.name}")
+
---
+++ nusantara/nusa_datasets/multilexnorm/multilexnorm.py
@@ -0,0 +1,172 @@
+from pathlib import Path
+from typing import Dict, List, Tuple
+
+import datasets
+
+from nusantara.utils import schemas
+from nusantara.utils.configs import NusantaraConfig
+from nusantara.utils.constants import Tasks
+
+_CITATION = """\
+@inproceedings{multilexnorm,
+  title= {MultiLexNorm: A Shared Task on Multilingual Lexical Normalization,
+  author = "van der Goot, Rob and Ramponi et al.",
+  booktitle = "Proceedings of the 7th Workshop on Noisy User-generated Text (W-NUT 2021)",
+  year = "2021",
+  publisher = "Association for Computational Linguistics",
+  address = "Punta Cana, Dominican Republic"
+}
+"""
+
+_DATASETNAME = "multilexnorm"
+
+_DESCRIPTION = """\
+MULTILEXNPRM is a new benchmark dataset for multilingual lexical normalization
+including 12 language variants,
+we here specifically work on the Indonisian-english language.
+"""
+
+_HOMEPAGE = "https://bitbucket.org/robvanderg/multilexnorm/src/master/"
+
+_LICENSE = "CC-BY-NC-SA 4.0"
+
+_URLS = {
+    "train": "https://bitbucket.org/robvanderg/multilexnorm/raw/e92e5b8f111fea15c7c88aebd4c058f6a1ca8d74/data/iden/train.norm",
+    "validation": "https://bitbucket.org/robvanderg/multilexnorm/raw/e92e5b8f111fea15c7c88aebd4c058f6a1ca8d74/data/iden/dev.norm",
+    "test": "https://bitbucket.org/robvanderg/multilexnorm/raw/e92e5b8f111fea15c7c88aebd4c058f6a1ca8d74/data/iden/test.norm",
+}
+
+_SUPPORTED_TASKS = [Tasks.MULTILEXNORM]
+
+_SOURCE_VERSION = "1.0.0"
+
+_NUSANTARA_VERSION = "1.0.0"
+
+
+class MultiLexNorm(datasets.GeneratorBasedBuilder):
+    """MultiLexNorm is a new benchmark dataset for lexical normalization for indonisian English language. which is the translation
+     of social media text to canonical text:
+    new pix      comming tomoroe
+    new pictures coming  tomorrow
+    """
+
+    SOURCE_VERSION = datasets.Version(_SOURCE_VERSION)
+    NUSANTARA_VERSION = datasets.Version(_NUSANTARA_VERSION)
+
+    BUILDER_CONFIGS = [
+        NusantaraConfig(
+            name="multilexnorm_source",
+            version=_SOURCE_VERSION,
+            description="multilexnorm source schema",
+            schema="source",
+            subset_id="multilexnorm",
+        ),
+        NusantaraConfig(
+            name="multilexnorm_nusantara_t2t",
+            version=_NUSANTARA_VERSION,
+            description="multilexnorm Nusantara schema",
+            schema="nusantara_t2t",
+            subset_id="multilexnorm",
+        ),
+    ]
+
+    DEFAULT_CONFIG_NAME = "multilexnorm_source"
+
+    def _info(self) -> datasets.DatasetInfo:
+
+        if self.config.schema == "source":
+
+            features = datasets.Features(
+                {
+                    "src_sent": datasets.Value("string"),
+                    "id": datasets.Value("string"),
+                    "norm_sent": datasets.Value("string"),
+                }
+            )
+
+        elif self.config.schema == "nusantara_t2t":
+            features = schemas.text2text_features
+
+        return datasets.DatasetInfo(
+            description=_DESCRIPTION,
+            features=features,
+            homepage=_HOMEPAGE,
+            license=_LICENSE,
+            citation=_CITATION,
+        )
+
+    def _split_generators(self, dl_manager: datasets.DownloadManager) -> List[datasets.SplitGenerator]:
+
+        train_path = Path(dl_manager.download_and_extract(_URLS["train"]))
+        validation_path = Path(dl_manager.download_and_extract(_URLS["validation"]))
+        test_path = Path(dl_manager.download_and_extract(_URLS["test"]))
+        data_files = {
+            "train": train_path,
+            "validation": validation_path,
+            "test": test_path,
+        }
+
+        return [
+            datasets.SplitGenerator(
+                name=datasets.Split.TRAIN,
+                gen_kwargs={
+                    "filepath": data_files["train"],
+                    "split": "train",
+                },
+            ),
+            datasets.SplitGenerator(
+                name=datasets.Split.TEST,
+                gen_kwargs={
+                    "filepath": data_files["test"],
+                    "split": "test",
+                },
+            ),
+            datasets.SplitGenerator(
+                name=datasets.Split.VALIDATION,
+                gen_kwargs={
+                    "filepath": data_files["validation"],
+                    "split": "dev",
+                },
+            ),
+        ]
+
+    def _generate_examples(self, filepath: Path, split: str) -> Tuple[int, Dict]:
+
+        curSent = []
+        print(filepath)
+        if self.config.schema == "source":
+            i = 0
+            for line in open(filepath):
+                tok = line.strip("\n").split("\t")
+
+                if tok == [""] or tok == []:
+                    ex = {"id": str(i), "src_sent": [x[0] for x in curSent], "norm_sent": [x[1] for x in curSent]}
+                    yield i, ex
+                    i += 1
+                    curSent = []
+
+                else:
+                    if len(tok) > 2:
+                        print("erroneous input, line:\n" + line + "\n in file " + filepath + " contains more then two elements")
+                    if len(tok) == 1:
+                        tok.append("")
+                    curSent.append(tok)
+
+        elif self.config.schema == "nusantara_t2t":
+            i = 0
+            for line in open(filepath):
+                tok = line.strip("\n").split("\t")
+
+                if tok == [""] or tok == []:
+                    ex = {"id": str(i), "text_1": [x[0] for x in curSent], "text_2": [x[1] for x in curSent], "text_1_name": "src_sent", "text_2_name": "norm_sent"}
+                    yield i, ex
+                    i += 1
+                    curSent = []
+
+                else:
+                    if len(tok) > 2:
+                        print("erroneous input, line:\n" + line + "\n in file " + filepath + " contains more then two elements")
+                    if len(tok) == 1:
+                        tok.append("")
+                    curSent.append(tok)
+
