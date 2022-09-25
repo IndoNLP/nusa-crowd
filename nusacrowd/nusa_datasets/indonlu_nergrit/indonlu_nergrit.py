53795bfa663ba3054962d8981fe202af38587031
Cahya Wirawan <cahya.wirawan@gmail.com>
2022-09-25 07:41:22 +0200

    Change import from nusantara to nusacrowd

--- nusacrowd/nusa_datasets/indonlu_nergrit/indonlu_nergrit.py
+++ nusacrowd/nusa_datasets/indonlu_nergrit/indonlu_nergrit.py
@@ -19,10 +19,10 @@ from typing import List
 
 import datasets
 
-from nusantara.utils import schemas
-from nusantara.utils.common_parser import load_conll_data
-from nusantara.utils.configs import NusantaraConfig
-from nusantara.utils.constants import Tasks
+from nusacrowd.utils import schemas
+from nusacrowd.utils.common_parser import load_conll_data
+from nusacrowd.utils.configs import NusantaraConfig
+from nusacrowd.utils.constants import Tasks
 
 _CITATION = """\
 """
