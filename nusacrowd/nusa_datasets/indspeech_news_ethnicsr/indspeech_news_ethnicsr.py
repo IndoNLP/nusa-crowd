14382e8b61bb22ff4d79e04028632caaaa94c29f
Samuel Cahyawijaya <scahyawijaya@connect.ust.hk>
2022-12-20 00:04:45 +0800

    Update indspeech_news_ethnicsr.py

--- nusacrowd/nusa_datasets/indspeech_news_ethnicsr/indspeech_news_ethnicsr.py
+++ nusacrowd/nusa_datasets/indspeech_news_ethnicsr/indspeech_news_ethnicsr.py
@@ -22,7 +22,7 @@ _CITATION = """\
 """
 
 _LOCAL = False
-_LANGUAGES = ["ind"]  # We follow ISO639-3 language code (https://iso639-3.sil.org/code_tables/639/data)
+_LANGUAGES = ["sun", "jav"]  # We follow ISO639-3 language code (https://iso639-3.sil.org/code_tables/639/data)
 _DATASETNAME = "indspeech_news_ethnicsr"
 
 _DESCRIPTION = """
