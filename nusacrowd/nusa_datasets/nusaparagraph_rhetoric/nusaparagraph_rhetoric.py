871ba552c224e14a6fa0110af9b7f69504cb189d
emmanueldavee <emmanuel.dave02@gmail.com>
2023-07-21 06:04:15 +0000

    fix citation

--- nusacrowd/nusa_datasets/nusaparagraph_rhetoric/nusaparagraph_rhetoric.py
+++ nusacrowd/nusa_datasets/nusaparagraph_rhetoric/nusaparagraph_rhetoric.py
@@ -14,17 +14,12 @@ _LANGUAGES = [
     "btk", "bew", "bug", "jav", "mad", "mak", "min", "mui", "rej", "sun"
 ]  # We follow ISO639-3 language code (https://iso639-3.sil.org/code_tables/639/data)
 _CITATION = """\
-@misc{winata2022nusax,
-      title={NusaX: Multilingual Parallel Sentiment Dataset for 10 Indonesian Local Languages},
-      author={Winata, Genta Indra and Aji, Alham Fikri and Cahyawijaya,
-      Samuel and Mahendra, Rahmad and Koto, Fajri and Romadhony,
-      Ade and Kurniawan, Kemal and Moeljadi, David and Prasojo,
-      Radityo Eko and Fung, Pascale and Baldwin, Timothy and Lau,
-      Jey Han and Sennrich, Rico and Ruder, Sebastian},
-      year={2022},
-      eprint={2205.15960},
-      archivePrefix={arXiv},
-      primaryClass={cs.CL}
+@unpublished{anonymous2023nusawrites:,        
+    title={NusaWrites: Constructing High-Quality Corpora for Underrepresented and Extremely Low-Resource Languages},        
+    author={Anonymous},        
+    journal={OpenReview Preprint},        
+    year={2023},        
+    note={anonymous preprint under review}    
 }
 """
 _DESCRIPTION = """\
--- nusacrowd/nusa_datasets/nusaparagraph_topic/nusaparagraph_topic.py
+++ nusacrowd/nusa_datasets/nusaparagraph_topic/nusaparagraph_topic.py
@@ -20,17 +20,12 @@ _LANGUAGES = [
 ]  # We follow ISO639-3 language code (https://iso639-3.sil.org/code_tables/639/data)
 
 _CITATION = """\
-@misc{winata2022nusax,
-      title={NusaX: Multilingual Parallel Sentiment Dataset for 10 Indonesian Local Languages},
-      author={Winata, Genta Indra and Aji, Alham Fikri and Cahyawijaya,
-      Samuel and Mahendra, Rahmad and Koto, Fajri and Romadhony,
-      Ade and Kurniawan, Kemal and Moeljadi, David and Prasojo,
-      Radityo Eko and Fung, Pascale and Baldwin, Timothy and Lau,
-      Jey Han and Sennrich, Rico and Ruder, Sebastian},
-      year={2022},
-      eprint={2205.15960},
-      archivePrefix={arXiv},
-      primaryClass={cs.CL}
+@unpublished{anonymous2023nusawrites:,        
+    title={NusaWrites: Constructing High-Quality Corpora for Underrepresented and Extremely Low-Resource Languages},        
+    author={Anonymous},        
+    journal={OpenReview Preprint},        
+    year={2023},        
+    note={anonymous preprint under review}    
 }
 """
 
