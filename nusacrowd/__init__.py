cb8eac264d1227e64837177766b4f229622d7899
Samuel Cahyawijaya <scahyawijaya@connect.ust.hk>
2023-07-30 14:57:42 +0800

    merge conflict with remote master

--- nusacrowd/__init__.py
+++ nusacrowd/__init__.py
@@ -1,8 +1,5 @@
-<<<<<<< Updated upstream
 from .config_helper import NusantaraMetadata, NusantaraConfigHelper, NusantaraMetadataHelper
 from .utils.constants import Tasks
-=======
 from .config_helper import list_datasets, load_dataset, load_datasets, list_benchmarks, load_benchmark
->>>>>>> Stashed changes
 
 __version__ = "0.1.0"
